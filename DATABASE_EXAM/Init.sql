2c7d00c18c6d7641efecdc1cb2ad4bf78967104a
Jamie G. Callan <jgc@techchapter.com>
2025-05-16 11:24:21 +0200

    added sharding mongodb server

--- DATABASE_EXAM/Init.sql
+++ DATABASE_EXAM/Init.sql
@@ -1,51 +1,45 @@
 CREATE TABLE IF NOT EXISTS weapon (
-    id CHAR(36) PRIMARY KEY DEFAULT (UUID()),
-    weapon_type CHAR(36),
+    id UUID PRIMARY KEY DEFAULT gen_random_uuid(),
+    weapon_type UUID,
     damage INT
 );
 
-
 CREATE TABLE IF NOT EXISTS inventory (
-    id CHAR(36) PRIMARY KEY DEFAULT (UUID()),
+    id UUID PRIMARY KEY DEFAULT gen_random_uuid(),
     gold INT NOT NULL
 );
 
-
 CREATE TABLE IF NOT EXISTS weapon_inventory (
-    inventory_id CHAR(36),
-    weapon_id CHAR(36),
+    inventory_id UUID,
+    weapon_id UUID,
     FOREIGN KEY (inventory_id) REFERENCES inventory(id),
     FOREIGN KEY (weapon_id) REFERENCES weapon(id)
-); 
-
+);
 
 CREATE TABLE IF NOT EXISTS player (
-    id CHAR(36) PRIMARY KEY DEFAULT (UUID()),
+    id UUID PRIMARY KEY DEFAULT gen_random_uuid(),
     name VARCHAR(100) NOT NULL,
     health INT NOT NULL,
-    inventory_id CHAR(36),
+    inventory_id UUID,
     FOREIGN KEY (inventory_id) REFERENCES inventory(id)
 );
 
-
 CREATE TABLE IF NOT EXISTS monster (
-    id CHAR(36) PRIMARY KEY DEFAULT (UUID()),
+    id UUID PRIMARY KEY DEFAULT gen_random_uuid(),
     name VARCHAR(100) NOT NULL,
     health INT NOT NULL
 );
 
 CREATE TABLE IF NOT EXISTS world (
-    id CHAR(36) PRIMARY KEY DEFAULT (UUID()),
-    lore_id char(36) not null,
-    player_id Char(36),
+    id UUID PRIMARY KEY DEFAULT gen_random_uuid(),
+    lore_id UUID NOT NULL,
+    player_id UUID,
     FOREIGN KEY (player_id) REFERENCES player(id)
 );
 
 CREATE TABLE IF NOT EXISTS world_monster (
-    world_id CHAR(36),
-    monster_id CHAR(36),
+    world_id UUID,
+    monster_id UUID,
     FOREIGN KEY (world_id) REFERENCES world(id),
     FOREIGN KEY (monster_id) REFERENCES monster(id)
 );
-
-
